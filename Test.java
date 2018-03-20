df9b3589b66123737615fa4ef5d7498cba78beda
SarahAbdl <37497000+SarahAbdl@users.noreply.github.com>
2018-03-20 09:15:22 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,15 +1,13 @@
 package billeterie;
 
-import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
 
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+import billeterie.ZDialogInfo; 
+
 
 
 
@@ -18,22 +16,31 @@ public class Test {
 	public static void main(String[] args) {
 		
 	Fenetre fn = new Fenetre ();
-		
-		try {
+	
+	try {
 	      Class.forName("org.postgresql.Driver");
 	      System.out.println("Driver O.K.");
 
-	      String url = "jdbc:postgresql://localhost:5432/billet";
+	      String url = "jdbc:postgresql://localhost:5432/billets";
 	      String user = "postgres";
 	      String passwd = "sarahabd";
+	      Connection conn = DriverManager.getConnection(url, user, passwd); 
 
-	      Connection conn = DriverManager.getConnection(url, user, passwd);
-	      System.out.println("Connexion effective !");         
-	         
-	    } catch (Exception e) {
-	      e.printStackTrace();
-	    }   		
-
+	      
+	      System.out.println("Connexion effective !");  
+	      
+	        Statement state = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+			
+//	        state.executeUpdate("INSERT INTO billets (civilite, prenom, nom, mail, concert, place) VALUES('"civilite.getText()
+//	        		+"','"+billets.getCivilite()+"','"+nom.getText()+"','"+mail.getText()+"','"+concert.getSelectedItem()+"','"+place.isSelected()+"')");
+//	        
+//	        		System.out.println("Informations rentrées dans la BDD");
+	      } 
+	 catch (Exception e) {
+		 e.printStackTrace();
+	 	}
+	}	
 	}
 
-}
+
+
