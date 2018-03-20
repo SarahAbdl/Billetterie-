d677ce30f5a34db6db430901dd8e6da1b11bca4e
SarahAbdl <37497000+SarahAbdl@users.noreply.github.com>
2018-03-20 11:51:21 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,4 +1,4 @@
-package billeterie;
+package billetterie;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 
-import billeterie.ZDialogInfo; 
+//import billeterie.ZDialogInfo; 
 
 
 
@@ -17,30 +17,11 @@ public class Test {
 		
 	Fenetre fn = new Fenetre ();
 	
-	try {
-	      Class.forName("org.postgresql.Driver");
-	      System.out.println("Driver O.K.");
-
-	      String url = "jdbc:postgresql://localhost:5432/billets";
-	      String user = "postgres";
-	      String passwd = "sarahabd";
-	      Connection conn = DriverManager.getConnection(url, user, passwd); 
-
-	      
-	      System.out.println("Connexion effective !");  
-	      
-	        Statement state = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
-			
-//	        state.executeUpdate("INSERT INTO billets (civilite, prenom, nom, mail, concert, place) VALUES('"civilite.getText()
-//	        		+"','"+billets.getCivilite()+"','"+nom.getText()+"','"+mail.getText()+"','"+concert.getSelectedItem()+"','"+place.isSelected()+"')");
-//	        
-//	        		System.out.println("Informations rentrées dans la BDD");
-	      } 
-	 catch (Exception e) {
-		 e.printStackTrace();
-	 	}
-	}	
 	}
+}
+
+
+
 
 
 
